aaa93dc3f47c55e1e6b9775afdc8a3b95822ea44
Andrew Ayetey <ayeteya@gmail.com>
2019-11-20 16:22:12 -0600

    figured it out, word was not self.word

--- Final/websterApi.py
+++ Final/websterApi.py
@@ -14,7 +14,15 @@ class WebsterWord:
             pass
         elif self.word != self.json[0]['hwi']['hw'].replace('*', ''):
             print("Word is not stem")
-            self.found = False
+            self.found = True
+            self.word = self.json[0]['hwi']['hw'].replace('*', '')
+            self.id = '17e1bacd-ab8e-4924-8c57-5a32357d29d9'
+            self.response = requests.get('https://www.dictionaryapi.com/api/v3/references/collegiate/json/{}?key={}'.format(self.word, self.id))
+            self.json = (self.response).json()
+            self.wordDic = self.json[0]
+            self.shortdef = self.wordDic['shortdef']
+            self.hwi = self.wordDic['hwi']
+            self.prsMw = self.hwi['prs'][0]['mw']
             pass
         else:
             self.found = True
